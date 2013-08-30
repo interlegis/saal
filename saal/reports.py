8de550d13fcb4c55b98613e28afe69d123b76be3
Sesostris Vieira <sesostris@interlegis.leg.br>
2013-08-30 09:53:51 -0300

    Melhorias nas classes de geração de relatórios.

--- saal/reports.py
+++ saal/reports.py
@@ -21,17 +21,18 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 #
-from django.utils.translation import ugettext_lazy as _
 from functools import update_wrapper
 import ho.pisa as pisa
 import cStringIO as StringIO
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import force_unicode, force_text
 from django.utils.safestring import mark_safe
-from django.views.generic import TemplateView, ListView
 from django.contrib import admin
 from django.contrib.admin.forms import forms
 from django.contrib.admin.options import IncorrectLookupParameters
+from django.views.generic import TemplateView, ListView
+from django.db import models
 
 def fetch_resources(uri, rel):
     path = rel + '/saal' + uri
@@ -39,6 +40,11 @@ def fetch_resources(uri, rel):
 
 class TemplatePdfView(TemplateView):
     pdf_template = None
+    pdf_file_name = None
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(request, context)
 
     def get_template_names(self):
         """
@@ -53,7 +59,14 @@ class TemplatePdfView(TemplateView):
         pdf = pisa.pisaDocument(src=StringIO.StringIO(html), encoding='utf8', link_callback=fetch_resources)
         if not pdf.err:
             response.content = pdf.dest.getvalue()
-            response['Content-Disposition'] = 'attachment; filename=%s.pdf' % self.opts.verbose_name.replace(' ', '_')
+            if self.pdf_file_name is None:
+                if hasattr(self, 'opts'):
+                    filename = self.opts.verbose_name
+                else:
+                    filename = 'report'
+            else:
+                filename = self.pdf_file_name
+            response['Content-Disposition'] = 'attachment; filename=%s.pdf' % filename.replace(' ', '_')
         return response
     
     def render_to_response(self, request, context, **response_kwargs):
@@ -137,14 +150,137 @@ class TabularListView(forms.Form, ListView):
     _app_label = None
     _title = None
     template_name_pdf = None
+    """
+    List of dicts that describes the fields to display. Each dict can have the format:
+    {'name': 'field_name', 'label': 'Label to head table', 'style': 'HTML style for browser rendering;', 'pdf_style': 'HTML style for PDF rendering;'}
+    For convenience, if instead a dict a string is found they will be replaced by a {'name': 'the_passed_str'} dict
+    """
     list_fields = None
+    _list_fields = None
+    """
+    List of dicts that describes grouping. Each dict can have the format:
+    { 'group_field': 'Control group-break field name',
+      'totals': [{field:'Field that need be totalized', label:'%(key)s %(value)s label to show the total', function: lambda x,y: Reduce calculation, },...]
+    }
+    Note: The 'totals' is a optional list of dicts with totalization descriptors.
+    """
     groups = []
+    """
+    List of dicts that describes report totalizations. Each dict can have the format:
+    {field:'Field that need be totalized', label:'%s label to show the total', function: lambda x,y: Reduce calculation, }
+    """
     totals = []
     page_size = "A4 portrait"
     margin_top = "4cm"
     margin_left = "2cm"
     margin_right = "2cm"
     margin_bottom = "4cm"
+    
+    def get_list_fields(self):
+        def get_label(field_name):
+            field_label = field_name.replace('__',' ').replace('_',' ').title()
+            if hasattr(self.model, '_meta'):
+                model = self.model
+                field_names = field_name.split('__')
+                for fn in field_names:
+                    try:
+                        f = model._meta.get_field(fn)
+                        field_label = f.verbose_name
+                        if hasattr(f, 'related'):
+                            model = f.related.parent_model
+                    except:
+                        return field_label
+            return _(field_label)
+
+        if self._list_fields is not None:
+            return self._list_fields
+
+        if self.list_fields is None:
+            if hasattr(self.model, '_meta'):
+                self.list_fields = self.model._meta.get_all_field_names()
+            else:
+                raise Exception(_(u'Report improperly configured. no list_fields property.'))
+        
+        if not isinstance(self.list_fields, (list, tuple)):
+            raise Exception(_(u'Report improperly configured. list_fields must be a list or tuple.'))
+        
+        self._list_fields = []
+        for field_def in self.list_fields:
+            if isinstance(field_def, basestring):
+                field_def = {'name': field_def, 'label': get_label(field_def), 'style': '', 'pdf_style': '', }
+            elif isinstance(field_def, dict):
+                if 'name' not in field_def:
+                    raise Exception(_(u'Report improperly configured. list_fields %s has no name element.' % field_def))
+                if 'label' not in field_def:
+                    field_def['label'] = get_label(field_def['name'])
+                if 'style' not in field_def:
+                    field_def['style'] = ''
+                if 'pdf_style' not in field_def:
+                    field_def['pdf_style'] = ''
+            else:
+                raise Exception(_(u"Report improperly configured. list_fields %s not recognized." % field_def))
+            self._list_fields.append(field_def)
+        
+        if not any(['width' in f for f in self._list_fields]):
+            if hasattr(self.model, '_meta'):
+                total = 0
+                for field in self._list_fields: 
+                    model = self.model
+                    field_names = field['name'].split('__')
+                    field_length = 20
+                    for fn in field_names:
+                        try:
+                            f = model._meta.get_field(fn)
+                            if f.max_length is not None:
+                                field_length = f.max_length
+                            elif hasattr(f, 'max_digits') and f.max_digits is not None:
+                                field_length = f.max_digits
+                            elif f.choices:
+                                field_length = max([len(c[1]) for c in f.choices])
+                            elif isinstance(f, (models.AutoField, models.IntegerField, models.FloatField, models.DateField,
+                                                models.TimeField)):
+                                field_length = 20
+                            elif isinstance(f, (models.BooleanField, models.NullBooleanField)):
+                                field_length = 10
+                            elif isinstance(f, models.TextField):
+                                field_length = 150
+                            if hasattr(f, 'related'):
+                                model = f.related.parent_model
+                                field_length = 100
+                        except:
+                            pass
+                    field['width'] = field_length
+                    total += field_length
+                for field in self._list_fields:
+                    field['width'] = '%d%%' % (100 * field['width'] / total) 
+                
+        return self._list_fields
+
+    def get_title(self):
+        if self._title is None:
+            if hasattr(self.model, '_meta'):
+                self._title = _(u'List of %s' % force_text(self.model._meta.verbose_name))
+            else:
+                self._title = _(u'Tabular list')
+        return self._title
+    
+    def set_title(self, value):
+        self._title = value
+        
+    title = property(get_title, set_title)
+    
+    def get_app_label(self):
+        if self._app_label is None:
+            if hasattr(self.model, '_meta'):
+                self._app_label = force_text(self.model._meta.app_label)
+            else:
+                self._app_label = 'unset'
+        return self._app_label
+    
+    def set_app_label(self, value):
+        self._app_label = value    
+    
+    app_label = property(get_app_label, set_app_label)
     
     def convert_to_pdf(self, response):
         html = response.content
@@ -224,58 +360,16 @@ class TabularListView(forms.Form, ListView):
             names = ['report/%s/tabular_list.html' % prefix] + names
 
         return names
-
-    def get_title(self):
-        if self._title is None:
-            if hasattr(self.model, '_meta'):
-                self._title = _(u'List of %s') % force_text(self.model._meta.verbose_name)
-            else:
-                self._title = _(u'Tabular list')
-        return self._title
-    
-    def set_title(self, value):
-        self._title = value
-        
-    title = property(get_title, set_title)
-    
-    def get_app_label(self):
-        if self._app_label is None:
-            if hasattr(self.model, '_meta'):
-                self._app_label = force_text(self.model._meta.app_label)
-            else:
-                self._app_label = 'unset'
-        return self._app_label
-    
-    def set_app_label(self, value):
-        self._app_label = value    
-    
-    app_label = property(get_app_label, set_app_label)
     
     def table_header(self):
-        def get_label(field):
-            if 'label' in field:
-                return field['label'] 
-            field['label'] = field['name'].replace('__',' ').replace('_',' ').title()
-            if hasattr(self.object_list, 'model'):
-                model = self.object_list.model
-                field_names = field['name'].split('__')
-                for fn in field_names:
-                    try:
-                        f = model._meta.get_field(fn)
-                        field['label'] = f.verbose_name
-                        if hasattr(f, 'related'):
-                            model = f.related.parent_model
-                    except:
-                        return field['label']
-            return field['label']
-        
         style_property = 'pdf_style' if self.request.GET.get('fmt', 'html') == 'PDF' else 'style'
         result = u""
-        for field in self.list_fields:
-            label = get_label(field)
-            if style_property not in field:
-                field[style_property] = ''
-            result += u'<th style="%s">%s</th>' % (field[style_property], label)
+        for field in self.get_list_fields():
+            label = field['label']
+            if 'width' in field:
+                result += u'<th width="%s" style="%s">%s</th>' % (field['width'], field[style_property], label)
+            else:
+                result += u'<th style="%s">%s</th>' % (field[style_property], label)
 
         return mark_safe(u"<tr>%s</tr>" % result)
     
@@ -284,17 +378,12 @@ class TabularListView(forms.Form, ListView):
             return u""
         style_property = 'pdf_style' if self.request.GET.get('fmt', 'html') == 'PDF' else 'style'
         result = u""
-        for field in self.list_fields:
-            if style_property not in field:
-                field[style_property] = ''
-            
+        for field in self.get_list_fields():
             value = u""
             for t in self.totals:
                 if t['field'] == field['name']:
                     value = t['label'] % t['value']
-                    
             result += u'<th style="%s">%s</th>' % (field[style_property], value)
-
         return mark_safe(u"<tr>%s</tr>" % result)
 
     def table_data(self):
@@ -309,6 +398,8 @@ class TabularListView(forms.Form, ListView):
                         obj = obj[fn]
                     except:
                         obj = u"??%s??" % fn
+                if callable(obj):
+                    obj = obj()
             return obj
                 
         style_property = 'pdf_style' if self.request.GET.get('fmt', 'html') == 'PDF' else 'style'
@@ -333,13 +424,10 @@ class TabularListView(forms.Form, ListView):
                     g['last_value'] = value
                 if g['last_value'] != value:
                     if not g['totals']:
-                        result += u'<tr><th colspan="%s">%s %s</th></tr>' % (len(self.list_fields), _(u'End of'), g['last_value'])
+                        result += u'<tr><th colspan="%s">%s %s</th></tr>' % (len(self.get_list_fields()), _(u'End of'), g['last_value'])
                     else:
                         row = u""
-                        for f in self.list_fields:
-                            if style_property not in f:
-                                f[style_property] = ''
-                            
+                        for f in self.get_list_fields():
                             v = u""
                             for t in g['totals']:
                                 if t['field'] == f['name']:
@@ -367,32 +455,22 @@ class TabularListView(forms.Form, ListView):
                 
             # Print data
             row = u""
-            for f in self.list_fields:
+            for f in self.get_list_fields():
                 value = get_value(obj, f)
-
-                if style_property not in f:
-                    f[style_property] = ''
-                
                 row += u'<td style="%s">%s</td>' % (f[style_property], value)
             result += u"<tr>%s</tr>" % row
             
         # Last group calculations
         for g in self.groups:
             if not g['totals']:
-                result += u'<tr><th colspan="%s">%s %s</th></tr>' % (len(self.list_fields), _(u'End of'), g['last_value'])
+                result += u'<tr><th colspan="%s">%s %s</th></tr>' % (len(self.get_list_fields()), _(u'End of'), g['last_value'])
             else:
                 row = u""
-                for f in self.list_fields:
-                    if style_property not in f:
-                        f[style_property] = ''
-                        
+                for f in self.get_list_fields():
                     v = u""
                     for t in g['totals']:
                         if t['field'] == f['name']:
                             v = t['label'] % {'key': g['last_value'], 'value': t['value']}
-                                
                     row += u'<th style="%s">%s</th>' % (f[style_property], v)
-            
                 result += u"<tr>%s</tr>" % row
-            
         return mark_safe(result)
